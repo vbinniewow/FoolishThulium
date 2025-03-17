155cc8297b5e86e7254a17c082d208e75cd08793
Vbinnie <vinicarvallhoyt@gmail.com>
2025-03-17 12:10:52 -0400

    1st Commit

---
+++ src/main/java/net/vbinnie/foolishthulium/FoolishThuliumClient.java
@@ -0,0 +1,10 @@
+package net.vbinnie.foolishthulium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoolishThuliumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/vbinnie/foolishthulium/FoolishThuliumDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vbinnie.foolishthulium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FoolishThuliumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
